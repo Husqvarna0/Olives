e69c60beef7b10cc0fe6d0018f4e0e7b526694e8
Husqvarna_0 <dancewithdead21@gmail.com>
2023-05-22 01:17:59 +0300

    add sign-up

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,14 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+
+let close = document.getElementById('sign-up__close');
+
+close.addEventListener("click", function () {
+	let signUp = document.getElementById("header__sign-up");
+	signUp.classList.add("__close");
+
+});
+
